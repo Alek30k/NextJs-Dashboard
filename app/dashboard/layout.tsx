e3aa34f0aa98e6e6e447c2c86fb793cd7477e60f
Alek30k <alemank23k@gmail.com>
2023-11-07 18:07:36 -0300

    last commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <section>
+      esto es el layout del dasboard
+      {children}
+    </section>
+  );
+}
