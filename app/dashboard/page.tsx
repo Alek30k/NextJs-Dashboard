7ed88067fedd8ab563b8045227c6ec95984f6e33
Alek30k <alemank23k@gmail.com>
2023-11-08 19:25:26 -0300

    last commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
+import { fetchRevenue } from "../lib/data";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  console.log(revenue);
+
   return <p>Esta página es el contenido del dashboard</p>;
 }
