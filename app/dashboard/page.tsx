1d218ce1ba4c7afa09c43fea24b6dc219b606307
Alek30k <alemank23k@gmail.com>
2023-11-09 16:53:49 -0300

    last commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,20 @@
 import { fetchRevenue } from "../lib/data";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import { lusitana } from "../ui/fonts";
+// import { fetchRevenue } from "../lib/data";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
 
-  console.log(revenue);
-
-  return <p>Esta página es el contenido del dashboard</p>;
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+      </div>
+    </main>
+  );
 }
