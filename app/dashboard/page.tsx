01f46f7bd45046aaec6579474b887f974c1bb40b
Alek30k <alemank23k@gmail.com>
2023-11-10 17:32:20 -0300

    last commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
@@ -15,7 +16,9 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense>
+          <RevenueChart revenue={revenue} />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
